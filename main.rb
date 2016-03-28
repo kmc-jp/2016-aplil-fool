37907e30c98e2d3e0566ae27f9623d9c04fad741
wass80 <wasss80@gmail.com>
2016-03-28 22:58:01 +0900

    work well

--- main.rb
+++ main.rb
@@ -1,2 +1,18 @@
 #! /usr/bin/env ruby
-puts $stdin.readline
+begin
+  request = $stdin.readline
+  method, uri, version = request.split(" ")
+  case uri
+  when "/"
+    puts <<EOS
+HTTP/1.0 200 OK
+
+<!doctype html>
+<p>work</p>
+EOS
+  else
+    puts "HTTP/1.0 404 Not Found"
+  end
+rescue => e
+  puts "HTTP/1.0 400 Bad Request"
+end
