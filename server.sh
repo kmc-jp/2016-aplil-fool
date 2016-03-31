0c35cd8e08df342a21c78d5050ea2f3ed0393474
wass80 <wasss80@gmail.com>
2016-03-31 22:31:29 +0900

    画像を追加

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 timeout=10
 port=18080
-socat -d -d -T $timeout TCP4-LISTEN:$port,fork,reuseaddr EXEC:./main.rb
+cd ultpiet/
+socat -d -d -T $timeout TCP4-LISTEN:$port,fork,reuseaddr EXEC:"./ultrapiet ../htmlserver.png"
